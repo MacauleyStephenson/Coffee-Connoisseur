cdba1360df9e85bd1789e89361d181b839b46fa9
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-12-10 18:21:11 +0000

    fixed _document.js cleaned up code

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,28 @@
-import Document, { Head, Main, Html, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
 				<Head>
-					<link rel="preload" href="/fonts/IBMPlexSans-Bold.ttf" as="font" crossOrigin="annonymous"></link>
-					<link rel="preload" href="/fonts/IBMPlexSans-Regular.ttf" as="font" crossOrigin="annonymous"></link>
-					<link rel="preload" href="/fonts/IBMPlexSans-SemiBold.ttf" as="font" crossOrigin="annonymous"></link>
+					<link
+						rel="preload"
+						href="/fonts/IBMPlexSans-Bold.ttf"
+						as="font"
+						crossOrigin="anonymous"
+					></link>
+					<link
+						rel="preload"
+						href="/fonts/IBMPlexSans-Regular.ttf"
+						as="font"
+						crossOrigin="anonymous"
+					></link>
+					<link
+						rel="preload"
+						href="/fonts/IBMPlexSans-SemiBold.ttf"
+						as="font"
+						crossOrigin="anonymous"
+					></link>
 				</Head>
 				<body>
 					<Main></Main>
