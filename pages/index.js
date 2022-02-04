a77eb83894c355c4778d51e8f25f561c0329bb66
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-04 16:43:27 +0000

    adding heading with Next.js functionality

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '../styles/Home.module.css';
 
 import Banner from '../components/banner';
 import Card from '../components/card';
+
 import coffeeStore from '../data/coffee-stores.json';
 
 export async function getStaticProps(context) {
+	console.log("Hi getSaticProps");
 	return {
 		props: {
 			coffeeStore,
@@ -35,18 +38,24 @@ export default function Home(props) {
 				<div className={styles.heroImage}>
 					<Image src="/static/hero-image.png" width={700} height={400} />
 				</div>
-				<div className={styles.cardLayout}>
-					{props.coffeeStore.map((coffeeStore) => {
-						return (
-							<Card
-								key={coffeeStore.id}
-								name={coffeeStore.name}
-								imgUrl={coffeeStore.imgUrl}
-								href={`/coffee-store/${coffeeStore.id}`}
-							/>
-						);
-					})}
-				</div>
+
+				{coffeeStore.length > 0 && (
+					<>
+						<h2 className={styles.heading2}>London Coffee Stores</h2>
+						<div className={styles.cardLayout}>
+							{props.coffeeStore.map((coffeeStore) => {
+								return (
+									<Card
+										key={coffeeStore.id}
+										name={coffeeStore.name}
+										imgUrl={coffeeStore.imgUrl}
+										href={`/coffee-store/${coffeeStore.id}`}
+									/>
+								);
+							})}
+						</div>
+					</>
+				)}
 			</main>
 		</div>
 	)
