84c1eb5cf1b31ac328e7e2723d4a65ea882e10aa
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-10 08:28:28 +0000

    adding Coffee Store page styling

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,11 @@
 import { useRouter } from "next/dist/client/router";
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 
 import CoffeeStoresData from '../../data/coffee-stores.json';
+
+import styles from '../../styles/coffee-store.module.css';
 
 export function getStaticProps(staticProps) {
 	const params = staticProps.params;
@@ -42,20 +45,32 @@ const CoffeeStore = (props) => {
 
 	console.log('props', props);
 	return (
-		<div>
+		<div className={styles.layout}>
 			<Head>
 				<title>{name}</title>
 			</Head>
-			Coffee Store Page {router.query.id}
-			<Link href="/">
-				<a>Back to home</a>
-			</Link>
-			<Link href="/coffee-store/dynamic">
-				<a>Go to page dynamic</a>
-			</Link>
-			<p>{address}</p>
-			<p>{name}</p>
-			<p>{neighbourhood}</p>
+			<div className={styles.col1}>
+				Coffee Store Page {router.query.id}
+				<Link href="/">
+					<a>Back to home</a>
+				</Link>
+				<Link href="/coffee-store/dynamic">
+					<a>Go to page dynamic</a>
+				</Link>
+				<p>{name}</p>
+				<Image
+				src={imgUrl}
+				width={60}
+				height={360}
+				className={styles.storeImg}
+				alt={name}>
+				</Image>
+			</div>
+
+			<div className={styles.col2}>
+				<p>{address}</p>
+				<p>{neighbourhood}</p>
+			</div>
 		</div>
 	);
 };
